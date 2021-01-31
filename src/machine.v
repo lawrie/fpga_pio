422188897f6556ccbeb83e701ad254ee0d56dbed
lawrie <lawrie.griffiths@ntlword.com>
2021-01-31 17:35:59 +0000

    Combinatorial instruction execution

--- src/machine.v
+++ src/machine.v
@@ -50,6 +50,8 @@ module machine (
   reg         decy;
   reg         set_shift;
   reg         do_shift;
+  reg         set_set_pins;
+  reg         set_set_dirs;
 
   reg         waiting = 0;
   reg [31:0]  new_val;
@@ -103,96 +105,106 @@ module machine (
   localparam IRQ  = 6;
   localparam SET  = 7;
 
-  // Execute the current instruction
+  // Count down if delay, and set pins
   always @(posedge clk) begin
-    if (reset) begin
-      delay_cnt <= 0;
-    end else if (en & penable) begin // Machine enabled and divided clock enabled
-      jmp <= 0;
-      pull <= 0;
-      push <= 0;
-      set_shift <= 0;
-      do_shift <= 0;
-      decx <= 0;
-      decy <= 0;
-      setx <= 0;
-      sety <= 0;
-      delay_cnt <= delay;
+    if (en & penable) begin
       if (delay_cnt > 0) delay_cnt <= delay_cnt - 1;
-      else begin
-        if (sideset_bits > 0) begin
-          if (pins_side_count > 4) output_pins[pins_side_base+4] <= side_set[4];
-          if (pins_side_count > 3) output_pins[pins_side_base+3] <= side_set[3];
-          if (pins_side_count > 2) output_pins[pins_side_base+2] <= side_set[2];
-          if (pins_side_count > 1) output_pins[pins_side_base+1] <= side_set[1];
-          if (pins_side_count > 0) output_pins[pins_side_base+0] <= side_set[0];
-        end
+      else delay_cnt <= delay;
+      if (sideset_bits > 0) begin
+        if (pins_side_count > 4) output_pins[pins_side_base+4] = side_set[4];
+        if (pins_side_count > 3) output_pins[pins_side_base+3] = side_set[3];
+        if (pins_side_count > 2) output_pins[pins_side_base+2] = side_set[2];
+        if (pins_side_count > 1) output_pins[pins_side_base+1] = side_set[1];
+        if (pins_side_count > 0) output_pins[pins_side_base+0] = side_set[0];
+      end
+      if (set_set_pins) begin
+         if (pins_set_count > 4) output_pins[pins_set_base+4] = op2[4];
+         if (pins_set_count > 3) output_pins[pins_set_base+3] = op2[3];
+         if (pins_set_count > 2) output_pins[pins_set_base+2] = op2[2];
+         if (pins_set_count > 1) output_pins[pins_set_base+1] = op2[1];
+         if (pins_set_count > 0) output_pins[pins_set_base+0] = op2[0];
+       end
+       if (set_set_dirs) begin
+         if (pins_set_count > 4) pin_directions[pins_set_base+4] = op2[4];
+         if (pins_set_count > 3) pin_directions[pins_set_base+3] = op2[3];
+         if (pins_set_count > 2) pin_directions[pins_set_base+2] = op2[2];
+         if (pins_set_count > 1) pin_directions[pins_set_base+1] = op2[1];
+         if (pins_set_count > 0) pin_directions[pins_set_base+0] = op2[0];
+       end
+    end
+  end
+  
+  // Execute the current instruction
+  always @* begin
+    begin
+      jmp  = 0;
+      pull = 0;
+      push = 0;
+      set_shift = 0;
+      do_shift = 0;
+      decx = 0;
+      decy = 0;
+      setx = 0;
+      sety = 0;
+      waiting = 0;
+      new_val = 0;
+      set_set_pins = 0;
+      set_set_dirs = 0;
+      begin
         case (op)
           JMP:  begin
-                  new_val[4:0] <= op2; 
+                  new_val[4:0] = op2; 
                   case (op1)
-                    0: jmp <= 1;
-                    1: jmp <= (x == 0);
-                    2: begin jmp <= (x != 0); decx <= 1; end
-                    3: jmp <= (y == 0);
-                    4: begin jmp <= (y != 0); decy <= 1; end
-                    5: jmp <= (x != y);
-                    6: jmp <= jmp_pin;
-                    7: jmp <= (out_shift != 0);
+                    0: jmp = 1;
+                    1: jmp = (x == 0);
+                    2: begin jmp = (x != 0); decx = 1; end
+                    3: jmp = (y == 0);
+                    4: begin jmp = (y != 0); decy = 1; end
+                    5: jmp = (x != y);
+                    6: jmp = jmp_pin;
+                    7: jmp = (out_shift != 0);
                   endcase
                 end
 	  WAIT: case (op1[1:0])
-                  0: waiting <= gpio_pins[op2] != op1[2];
-                  1: waiting <= input_pins[op2] != op1[2];
-                  2: waiting <= irq_flags[op2] != op1[2];
+                  0: waiting = gpio_pins[op2] != op1[2];
+                  1: waiting = input_pins[op2] != op1[2];
+                  2: waiting = irq_flags[op2] != op1[2];
                 endcase
           IN:   case (op1)
-                  1: begin new_val <= in_shift; setx <= 1; end
+                  1: begin new_val = in_shift; setx = 1; end
                 endcase
           OUT:  case (op1)
                   0: begin end // Pins
-                  1: begin do_shift <= 1; new_val <= out_shift; setx <= 1; end // X
+                  1: begin do_shift = 1; new_val = out_shift; setx = 1; end // X
                   2: begin end // Y
                   4: begin end // Pindirs
                   5: begin end // PC
                 endcase
           PUSH: if (!op1[2]) begin end                                               // Push
                 //else begin pull <= 1; set_shift <= 1; waiting <= op[0] && empty; end // Pull
-                else begin pull <= 1; set_shift <= 1; waiting <= empty; end // Pull
+                else begin pull = 1; set_shift = 1; waiting = empty; end // Pull
           MOV:  case (op1)
                   0: begin end // Pins
                   1: case (op2[2:0]) // X
-                       2: begin new_val <= bit_op(y, op2[4:3]); setx <= 1; end // Y
+                       2: begin new_val = bit_op(y, op2[4:3]); setx = 1; end // Y
                      endcase
                   2: case (op2[2:0]) // Y
-                       1: begin new_val <= bit_op(x, op2[4:3]); sety <= 1; end // X
+                       1: begin new_val = bit_op(x, op2[4:3]); sety = 1; end // X
                      endcase
                   4: begin end // Exec
                   5: case (op2[2:0]) // PC
-                       1: begin new_val <= bit_op(x, op2[4:3]); jmp <= 1; end // X
-                       2: begin new_val <= bit_op(y, op2[4:3]); jmp <= 1; end // Y
+                       1: begin new_val = bit_op(x, op2[4:3]); jmp = 1; end // X
+                       2: begin new_val = bit_op(y, op2[4:3]); jmp = 1; end // Y
                      endcase
                   6: begin end // ISR
                   7: begin end // OSR
                 endcase
           IRQ:  begin end
           SET:  case (op1)
-                  0: begin
-                       if (pins_set_count > 4) output_pins[pins_set_base+4] <= op2[4];
-                       if (pins_set_count > 3) output_pins[pins_set_base+3] <= op2[3];
-                       if (pins_set_count > 2) output_pins[pins_set_base+2] <= op2[2];
-                       if (pins_set_count > 1) output_pins[pins_set_base+1] <= op2[1];
-                       if (pins_set_count > 0) output_pins[pins_set_base+0] <= op2[0];
-                     end
-                  1: begin setx <= 1; new_val <= {27'b0, op2}; end
-                  2: begin sety <= 1; new_val <= {27'b0, op2}; end
-                  4: begin
-                       if (pins_set_count > 4) pin_directions[pins_set_base+4] <= op2[4];
-                       if (pins_set_count > 3) pin_directions[pins_set_base+3] <= op2[3];
-                       if (pins_set_count > 2) pin_directions[pins_set_base+2] <= op2[2];
-                       if (pins_set_count > 1) pin_directions[pins_set_base+1] <= op2[1];
-                       if (pins_set_count > 0) pin_directions[pins_set_base+0] <= op2[0];
-                     end
+                  0: set_set_pins = 1;
+                  1: begin setx = 1; new_val = {27'b0, op2}; end
+                  2: begin sety = 1; new_val = {27'b0, op2}; end
+                  4: set_set_dirs = 1;
                 endcase
         endcase
       end
