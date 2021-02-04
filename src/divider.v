b7981587088fa66db565f5efe11f6766b113dfb4
lawrie <lawrie.griffiths@ntlword.com>
2021-02-04 10:53:29 +0000

    Corrected fractional divider

--- src/divider.v
+++ src/divider.v
@@ -6,19 +6,19 @@ module divider (
   output reg    penable
 );
 
-  reg [31:0] div_counter;
+  reg [23:0] div_counter;
 
   always @(posedge clk) begin
     if (reset) begin
       div_counter <= 0;
       penable <= 1;
     end else begin
-      if (div <= 23'h100) // normal clock if divider 1 or less
+      if (div < 23'h200) // normal clock if divider less than 2
         penable <= 1;
       else begin
         div_counter <= div_counter + 256;
-        if (div_counter >= div -1)
-          div_counter <= 0;
+        if (div_counter >= div - 256)
+          div_counter <= div_counter - (div - 256);
         penable <= div_counter < (div >> 1);
       end
     end
