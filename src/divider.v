9cab6a1fe9f5782fb5977ca8180fe311d2b57d9a
buncram <cramium@kosagi.com>
2023-05-05 01:15:41 +0800

    fix divider edge cases
    tested for the following ratios:
    
    0.0 (which maps to 1.0)
    1.0
    1.5
    1.25
    1.75
    2.0
    2.5
    
    The main issue is the ratios in the range of 1.0 <= ratio < 2.0
    were broken due to some edge cases not being handled correctly.
    
    This went through a manual merge to fix some module interfaces
    that had already drifted from my base branch, hopefully I patched
    everything up correctly...

--- src/divider.v
+++ src/divider.v
@@ -3,25 +3,35 @@
 
 `default_nettype none
 module divider (
-  input         clk,
-  input         reset,
-  input [23:0]  div,
-  input         use_divider,
-  output        penable,
-  output        pclk
+  input        clk,
+  input        reset,
+  input [23:0] div,
+  input        use_divider,
+  output       penable,
+  output       pclk
 );
+  wire [15:0] div_int;
+  wire [7:0] div_frac;
+  assign div_int = div[23:8];
+  assign div_frac = div[7:0];
+ 
+  wire not_zero_div;
+  wire divint_1;
+  assign not_zero_div = !((div_int == 0) && (div_frac == 0));
+  assign divint_1 = (div_int == 16'd1);
 
   reg [23:0] div_counter;
-  reg        pen;
-  reg        old_pen;
+  reg pen;
+  reg old_pen;
 
+  assign div = {div_int, div_frac};
   always @(posedge clk) begin
     if (reset) begin
       div_counter <= 0;
       pen <= 1;
       old_pen <= 0;
     end else begin
-      if (use_divider) begin
+      if (not_zero_div) begin
         old_pen <= pen;
         div_counter <= div_counter + 256;
         if (div_counter >= div - 256)
@@ -31,7 +41,5 @@ module divider (
     end
   end
 
-  assign penable = pen & ~old_pen;
-  assign pclk = pen;
-
+  assign penable = ((pen & ~old_pen) || !not_zero_div) ^ divint_1;
 endmodule
